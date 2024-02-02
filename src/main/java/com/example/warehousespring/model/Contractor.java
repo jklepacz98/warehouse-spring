15615831fc63fb5b1fa3650a0301b89d4bee2dcc
jklepacz98 <jakubklepacz98@gmail.com>
2024-02-02 14:10:38 +0100

    Added Contractor model

---
+++ src/main/java/com/example/warehousespring/model/Contractor.java
@@ -0,0 +1,42 @@
+package com.example.warehousespring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Contractor {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String symbol;
+    private String name;
+
+    Contractor(){}
+
+    public Contractor(String symbol, String name) {
+        this.symbol = symbol;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
