9457da4648b69431a2b728bec277e62539e5d408
jklepacz98 <jakubklepacz98@gmail.com>
2024-02-08 00:29:47 +0100

    Changed preloaded data

--- src/main/java/com/example/warehousespring/LoadDatabase.java
+++ src/main/java/com/example/warehousespring/LoadDatabase.java
@@ -26,14 +26,40 @@ public class LoadDatabase {
             DocumentItemReposiotory documentItemReposiotory
     ) {
         return args -> {
-            Contractor contractor1 = new Contractor("symbol1", "name1");
-            Contractor contractor2 = new Contractor("symbol2", "name2");
+            Contractor contractor1 = new Contractor("ASD/213", "John Smith");
+            Contractor contractor2 = new Contractor("PWQ/4563", "Jan Kowalski");
+            Contractor contractor3 = new Contractor("LDS/143", "Barbara Nowak");
+            Contractor contractor4 = new Contractor("JGS/4583", "Małgorzata Bubel");
+            Contractor contractor5 = new Contractor("EDC/453", "Bożydar Kowalski");
             log.info("Preloading " + contractorRepository.save(contractor1));
             log.info("Preloading " + contractorRepository.save(contractor2));
-            ReceiptDocument receiptDocument1 = new ReceiptDocument("symbol1", contractor1, Collections.emptyList());
+            log.info("Preloading " + contractorRepository.save(contractor3));
+            log.info("Preloading " + contractorRepository.save(contractor4));
+            log.info("Preloading " + contractorRepository.save(contractor5));
+            ReceiptDocument receiptDocument1 = new ReceiptDocument("DSFF/1234/32153", contractor1, Collections.emptyList());
+            ReceiptDocument receiptDocument2 = new ReceiptDocument("IFA/321/654", contractor1, Collections.emptyList());
+            ReceiptDocument receiptDocument3 = new ReceiptDocument("FJKL/321/435", contractor1, Collections.emptyList());
+            ReceiptDocument receiptDocument4 = new ReceiptDocument("JJKL/321/43", contractor1, Collections.emptyList());
+            ReceiptDocument receiptDocument5 = new ReceiptDocument("SAJK/943/1", contractor2, Collections.emptyList());
+            ReceiptDocument receiptDocument6 = new ReceiptDocument("LSDA/3214", contractor3, Collections.emptyList());
             log.info("Preloading " + receiptDocumentRepository.save(receiptDocument1));
-            DocumentItem documentItem1 = new DocumentItem("productName1", "kg", 100.0F, receiptDocument1.getId());
+            log.info("Preloading " + receiptDocumentRepository.save(receiptDocument2));
+            log.info("Preloading " + receiptDocumentRepository.save(receiptDocument3));
+            log.info("Preloading " + receiptDocumentRepository.save(receiptDocument4));
+            log.info("Preloading " + receiptDocumentRepository.save(receiptDocument5));
+            log.info("Preloading " + receiptDocumentRepository.save(receiptDocument6));
+            DocumentItem documentItem1 = new DocumentItem("Butter", "g", 10.0F, receiptDocument1.getId());
+            DocumentItem documentItem2 = new DocumentItem("Milk", "l", 1.0F, receiptDocument1.getId());
+            DocumentItem documentItem3 = new DocumentItem("Eggs", "kg", 0.5F, receiptDocument1.getId());
+            DocumentItem documentItem4 = new DocumentItem("Flour", "kg", 100.0F, receiptDocument1.getId());
+            DocumentItem documentItem5 = new DocumentItem("Rice", "kg", 0.4F, receiptDocument1.getId());
+            DocumentItem documentItem6 = new DocumentItem("water", "l", 5.0F, receiptDocument2.getId());
             log.info("Preloading " + documentItemReposiotory.save(documentItem1));
+            log.info("Preloading " + documentItemReposiotory.save(documentItem2));
+            log.info("Preloading " + documentItemReposiotory.save(documentItem3));
+            log.info("Preloading " + documentItemReposiotory.save(documentItem4));
+            log.info("Preloading " + documentItemReposiotory.save(documentItem5));
+            log.info("Preloading " + documentItemReposiotory.save(documentItem6));
         };
     }
 }
